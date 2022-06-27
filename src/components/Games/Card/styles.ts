e0b7b428b4020a0a7d6a7540f9e7a76496ec5827
Giankaz <91921192+Giankaz@users.noreply.github.com>
2022-06-27 05:17:51 -0300

    fix

--- src/components/Games/Card/styles.ts
+++ src/components/Games/Card/styles.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import skybg from '../../../assets/skybg.jpg'
-import ground from '../../../assets/ground.webp'
+import ground from '../../../assets/newground.png'
 
 interface DivProps {
 	bgImage?: string;
